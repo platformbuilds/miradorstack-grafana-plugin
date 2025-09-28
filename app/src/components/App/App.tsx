d0a87bff474465ce94a843c49c460246bb403f2a
aarvee11 <rv@platformbuilds.org>
2025-09-28 17:47:40 +0530

    fixing integration tests

--- app/src/components/App/App.tsx
+++ app/src/components/App/App.tsx
@@ -1,21 +1,19 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { AppRootProps } from '@grafana/data';
 import { ROUTES } from '../../constants';
-const DiscoverPage = React.lazy(() => import('../../pages/Discover'));
-const SchemaPage = React.lazy(() => import('../../pages/Schema'));
+import DiscoverPage from '../../pages/Discover';
+import SchemaPage from '../../pages/Schema';
 
 function App(props: AppRootProps) {
   return (
-    <Suspense fallback={null}>
-      <Routes>
-        <Route path={ROUTES.Discover} element={<DiscoverPage {...props} />} />
-        <Route path={ROUTES.Schema} element={<SchemaPage {...props} />} />
+    <Routes>
+      <Route path={ROUTES.Discover} element={<DiscoverPage {...props} />} />
+      <Route path={ROUTES.Schema} element={<SchemaPage {...props} />} />
 
-        {/* Default page */}
-        <Route path="*" element={<DiscoverPage {...props} />} />
-      </Routes>
-    </Suspense>
+      {/* Default page */}
+      <Route path="*" element={<DiscoverPage {...props} />} />
+    </Routes>
   );
 }
 
--- app/src/module.tsx
+++ app/src/module.tsx
@@ -1,22 +1,12 @@
-import React, { Suspense, lazy } from 'react';
+import React from 'react';
 import { AppPlugin, type AppRootProps } from '@grafana/data';
-import { LoadingPlaceholder } from '@grafana/ui';
 import type { AppConfigProps } from './components/AppConfig/AppConfig';
+import AppRoot from './components/App/App';
+import AppConfigRoot from './components/AppConfig/AppConfig';
 
-const LazyApp = lazy(() => import('./components/App/App'));
-const LazyAppConfig = lazy(() => import('./components/AppConfig/AppConfig'));
+const App = (props: AppRootProps) => <AppRoot {...props} />;
 
-const App = (props: AppRootProps) => (
-  <Suspense fallback={<LoadingPlaceholder text="" />}>
-    <LazyApp {...props} />
-  </Suspense>
-);
-
-const AppConfig = (props: AppConfigProps) => (
-  <Suspense fallback={<LoadingPlaceholder text="" />}>
-    <LazyAppConfig {...props} />
-  </Suspense>
-);
+const AppConfig = (props: AppConfigProps) => <AppConfigRoot {...props} />;
 
 export const plugin = new AppPlugin<{}>().setRootPage(App).addConfigPage({
   title: 'Configuration',
